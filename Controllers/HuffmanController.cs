3936f0af687c426998d26fc79ee592feb4167411
KEVIN ORTIZ RAMIREZ <kortiz@correo.url.edu.gt>
2020-03-02 15:39:45 -0600

    Recibo de archivo

--- Controllers/HuffmanController.cs
+++ Controllers/HuffmanController.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System;
 using System.Linq;
 using System.Web;
 using System.IO;
 using System.Text;
 using Microsoft.AspNetCore.Mvc;
+using EDII_Lab03.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace EDII_Lab03.Controllers
 {
@@ -12,6 +15,30 @@ namespace EDII_Lab03.Controllers
     [ApiController]
     public class HuffmanController : ControllerBase
     {
+        const int bufferLength = 100;
+        public List<Caracteres> ListaCaracteresExistentes = new List<Caracteres>();
+        public int totalCaracteres;
+        public Dictionary<string, byte> diccionarioIndices = new Dictionary<string, byte>();
+        public string txtBin = "";
+        public string nombreArchivo;
 
+        public async Task<IActionResult> OnPostUploadAsync(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var filePath = Path.GetTempFileName();
+
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+            return Ok(new { count = files.Count, size });
+        }
     }
 }
---
+++ Models/Caracteres.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EDII_Lab03.Models
+{
+    public class Caracteres : IComparable
+    {
+        public byte CaracterTexto { get; set; }
+        public int Frecuencia { get; set; }
+        public int indice { get; set; }
+        public bool CaracterYaRecorrido = false;
+        public bool CaracterAUsar = false;
+        public string binarioText { get; set; }
+        public int CompareTo(object obj)
+        {
+            var vComparador = (Caracteres)obj;
+            return CaracterTexto.CompareTo(vComparador.CaracterTexto);
+        }
+    }
+}
