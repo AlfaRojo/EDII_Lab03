05b54477f0a17add9bf41613e5096920d199634a
PabloArreaga <paal241199@gmail.com>
2020-03-08 04:39:29 -0600

    Se agrego metodo para contar la frecuencia de cada caracter

--- ArbolHuff/Huffman.cs
+++ ArbolHuff/Huffman.cs
@@ -2,11 +2,51 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text;
+using System.IO;
 
 namespace EDII_Lab03.ArbolHuff
 {
-    public class Huffman
-    {
-        //Arbol Huffman
-    }
+	public class Huffman
+	{
+		List<Nodo> frecuencias = new List<Nodo>();
+		//
+
+		public int Leer(string direccion)
+		{
+			var noCaracteres = 0;
+			// metodo para leer archivo, que incremente el contador y llame al metodo "ConteoDeFrecuencia"
+			return noCaracteres;
+		}
+
+		public void ConteoDeFrecuencia(byte elemento)
+		{
+			int posicionLista;
+			if (frecuencias.Exists(x => x.caracter == elemento))
+			{
+				posicionLista = frecuencias.FindIndex(x => x.caracter == elemento);
+
+				Nodo prueba = new Nodo();
+				prueba = frecuencias.Find(x => x.caracter == elemento);
+				frecuencias.RemoveAt(posicionLista);
+				frecuencias.Add(new Nodo()
+				{
+					caracter = elemento,
+					probabilidad = prueba.probabilidad + 1
+				});
+
+			}
+			else
+			{
+				frecuencias.Add(new Nodo()
+				{
+					caracter = elemento,
+					probabilidad = 1
+				});
+			}
+		}
+
+
+
+	}
 }
--- Models/Caracteres.cs
+++ Models/Caracteres.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace EDII_Lab03.Models
 {
     public class Caracteres : IComparable
