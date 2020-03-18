c6330b6c0810132382fb7364311764fafb25081e
KEVIN ORTIZ RAMIREZ <kortiz@correo.url.edu.gt>
2020-03-17 23:07:26 -0600

    Solucionando return archivo

--- LZW/CompressLZW.cs
+++ LZW/CompressLZW.cs
@@ -13,20 +13,20 @@ namespace EDII_Lab03.LZW
     {
         public int BufferSize { get; private set; }
         const int bufferLength = 100;
-        public List<Caracteres> ListaCaracteresExistentes = new List<Caracteres>();
-        public int TotalDeCaracteres;
+        public List<Caracteres> caracteresExistentes = new List<Caracteres>();
+        public int totalCaracteres;
         public Dictionary<string, byte> DiccionarioIndices = new Dictionary<string, byte>();
         public string TextoEnBinario = "";
         public string nombreArchivo;
 
-        public ActionResult CompresionLZWImportar(FileStream ArchivoImportado)
+        public void CompresionLZWImportar(FileStream ArchivoImportado)
         {
-            Dictionary<string, int> DiccionarioLZWCompresion = new Dictionary<string, int>();
-            Directory.CreateDirectory("~/App_Data/ArchivosImportados/");
+            Dictionary<string, int> LZWdiccionario = new Dictionary<string, int>();
+            Directory.CreateDirectory("~/App_Data/Importados/");
             Directory.CreateDirectory("~/App_Data/Compresiones/");
             var DireccionArchivo = string.Empty;
-            var ArchivoMapeo = "~/App_Data/ArchivosImportados/";
-            DireccionArchivo = ArchivoMapeo + Path.GetFileName(ArchivoImportado.Name);
+            var archivoDireccion = "~/App_Data/Importados/";
+            DireccionArchivo = archivoDireccion + Path.GetFileName(ArchivoImportado.Name);
             var extension = Path.GetExtension(ArchivoImportado.Name);
             var PropiedadesArchivoActual = new MiArchivo();
             FileInfo ArchivoAnalizado = new FileInfo(DireccionArchivo);
@@ -34,7 +34,6 @@ namespace EDII_Lab03.LZW
             PropiedadesArchivoActual.NombreArchivoOriginal = ArchivoAnalizado.Name;
             nombreArchivo = ArchivoAnalizado.Name.Split('.')[0];
             var listaCaracteresExistentes = new List<byte>();
-            var listaCaracteresEscribir = new List<int>();
             var listaCaracteresBinario = new List<string>();
             using (var Lectura = new BinaryReader(ArchivoImportado))
             {
@@ -58,86 +57,92 @@ namespace EDII_Lab03.LZW
                         foreach (var item in listaCaracteresExistentes)
                         {
                             var caractreres = Convert.ToChar(item);
-                            DiccionarioLZWCompresion.Add(caractreres.ToString(), DiccionarioLZWCompresion.Count + 1);
+                            LZWdiccionario.Add(caractreres.ToString(), LZWdiccionario.Count + 1);
                         }
-                        var TamanoDiccionario = Convert.ToString(DiccionarioLZWCompresion.LongCount()) + ".";
-                        writer.Write(TamanoDiccionario.ToCharArray());
+                        var diccionarioTam = Convert.ToString(LZWdiccionario.LongCount()) + ".";
+                        writer.Write(diccionarioTam.ToCharArray());
                         Lectura.BaseStream.Position = 0;
-                        var CaracterActual = string.Empty;
-                        var Output = string.Empty;
+                        var thisCaracter = string.Empty;
+                        var myOutput = string.Empty;
+                        var ASCIIescribir = new List<int>();
                         while (Lectura.BaseStream.Position != Lectura.BaseStream.Length)
                         {
                             byteBuffer = Lectura.ReadBytes(bufferLength);
                             foreach (byte item in byteBuffer)
                             {
-                                var CadenaAnalizada = CaracterActual + Convert.ToChar(item);
-                                if (DiccionarioLZWCompresion.ContainsKey(CadenaAnalizada))
+                                var toAnalizar = thisCaracter + Convert.ToChar(item);
+                                if (LZWdiccionario.ContainsKey(toAnalizar))
                                 {
-                                    CaracterActual = CadenaAnalizada;
+                                    thisCaracter = toAnalizar;
                                 }
                                 else
                                 {
-                                    listaCaracteresEscribir.Add(DiccionarioLZWCompresion[CaracterActual]);
-                                    DiccionarioLZWCompresion.Add(CadenaAnalizada, DiccionarioLZWCompresion.Count + 1);
-                                    CaracterActual = Convert.ToChar(item).ToString();
+                                    ASCIIescribir.Add(LZWdiccionario[thisCaracter]);
+                                    LZWdiccionario.Add(toAnalizar, LZWdiccionario.Count + 1);
+                                    thisCaracter = Convert.ToChar(item).ToString();
                                 }
                             }
                         }
-                        listaCaracteresEscribir.Add(DiccionarioLZWCompresion[CaracterActual]);
-                        var TamanoTexto = Convert.ToString(DiccionarioLZWCompresion.LongCount()) + ".";
-                        writer.Write(TamanoTexto.ToCharArray());
+                        ASCIIescribir.Add(LZWdiccionario[thisCaracter]);
+                        var textotamano = Convert.ToString(LZWdiccionario.LongCount()) + ".";
+                        writer.Write(textotamano.ToCharArray());
                         foreach (var item in listaCaracteresExistentes)
                         {
                             var Indice = Convert.ToByte(item);
                             writer.Write(Indice);
                         }
                         writer.Write(Environment.NewLine);
-                        var mayorIndice = listaCaracteresEscribir.Max();
-                        var bitsMayorIndice = (Convert.ToString(mayorIndice, 2)).Count();
-                        writer.Write(bitsMayorIndice.ToString().ToCharArray());
+                        var mayorIndice = ASCIIescribir.Max();
+                        var bitsIndiceMayor = (Convert.ToString(mayorIndice, 2)).Count();
+                        writer.Write(bitsIndiceMayor.ToString().ToCharArray());
                         writer.Write(extension.ToCharArray());
                         writer.Write(Environment.NewLine);
                         if (mayorIndice > 255)
                         {
-                            foreach (var item in listaCaracteresEscribir)
+                            foreach (var item in ASCIIescribir)
                             {
                                 var indiceBinario = Convert.ToString(item, 2);
-                                while (indiceBinario.Count() < bitsMayorIndice)
+                                while (indiceBinario.Count() < bitsIndiceMayor)
                                 {
                                     indiceBinario = "0" + indiceBinario;
                                 }
                                 listaCaracteresBinario.Add(indiceBinario);
                             }
-                            var cadenaBits = string.Empty;
+                            var allBits = string.Empty;
                             foreach (var item in listaCaracteresBinario)
                             {
                                 for (int i = 0; i < item.Length; i++)
                                 {
-                                    if (cadenaBits.Count() < 8)
+                                    if (allBits.Count() < 8)
                                     {
-                                        cadenaBits += item[i];
+                                        allBits += item[i];
                                     }
                                     else
                                     {
-                                        var cadenaDecimal = Convert.ToInt64(cadenaBits, 2);
-                                        var cadenaEnByte = Convert.ToByte(cadenaDecimal);
-                                        writer.Write((cadenaEnByte));
-                                        cadenaBits = string.Empty;
-                                        cadenaBits += item[i];
+                                        var allDecimal = Convert.ToInt64(allBits, 2);
+                                        var allBytes = Convert.ToByte(allDecimal);
+                                        writer.Write((allBytes));
+                                        allBits = string.Empty;
+                                        allBits += item[i];
                                     }
                                 }
                             }
-                            if (cadenaBits.Length > 0)
+                            if (allBits.Length > 0)
                             {
-                                var cadenaRestante = Convert.ToInt64(cadenaBits, 2);
-                                writer.Write(Convert.ToByte(cadenaRestante));
+                                var allResultado = Convert.ToInt64(allBits, 2);
+                                writer.Write(Convert.ToByte(allResultado));
                             }
                         }
                         else
                         {
-                            foreach (var item in listaCaracteresEscribir)
+                            foreach (var item in ASCIIescribir)
                             {
                                 writer.Write(Convert.ToByte(Convert.ToInt32(item)));
+                                using (FileStream archivoFinal = new FileStream((@"~/App_Data/Compresiones/" + nombreArchivo + ".lzw"), FileMode.OpenOrCreate))
+                                {
+                                    archivoFinal.WriteByte(Convert.ToByte(Convert.ToInt32(item)));
+                                }
+                                //Escribir los bytes en el archivo
                             }
                         }
                         PropiedadesArchivoActual.TamanoArchivoComprimido = writeStream.Length;
@@ -150,11 +155,6 @@ namespace EDII_Lab03.LZW
                 }
             }
             var FileVirtualPath = @"~/App_Data/Compresiones/" + nombreArchivo + ".lzw";
-            return new FileStreamResult(FileVirtualPath, "application/x-rar-compressed")//Revisar archivo retornado
-            {
-                FileDownloadName = "file.lzw"
-            };
-
         }
         #region Utilizables
         void GuaradarCompresiones(MiArchivo Archivo)
